0bd576f11d90ee3da53b4d80e2b55eaeeebbf58e
Maryline <marybot@free.fr>
2023-07-24 15:32:24 +0200

    add news app to settings

--- collecjv/collecjv/settings.py
+++ collecjv/collecjv/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "collecjv"
+    "collecjv",
+    "search",
+    "login",
+    "collection",
 ]
 
 MIDDLEWARE = [
