91759afebb36a7fc50caf6be844b1cd9043d3807
Maryline <marybot@free.fr>
2023-12-28 18:04:22 +0100

    update tests models

---
+++ collecjv/collecjv/tests.py
@@ -0,0 +1,16 @@
+import pytest
+from django.test import TestCase
+from collecjv.models import Company
+
+
+@pytest.mark.django_db
+class TestCompagny(TestCase):
+    compagny = Company.objects.create(
+        name="test compagny",
+        area="europe",
+        developer=True,
+        editor=True
+
+    )
+    expected_value = "test compagny"
+    assert str(compagny.name) == expected_value
--- collecjv/login/tests.py
+++ collecjv/login/tests.py
@@ -3,10 +3,11 @@ import pytest
 from django.test import Client, TestCase
 from login.models import PassChange
 
+
 @pytest.mark.django_db
 class TestLoginModel(TestCase):
     def test_passchange(self):
-        client = Client()
+
         passchange = PassChange.objects.create(
             email="test@test.fr",
             token="Dsojfpsosetijpjpzers",
@@ -23,7 +24,7 @@ class TestLoginViews(TestCase):
         
     def test_get(self):
         """views.get_context_data() sets 'name' in context."""
-         # Setup request and view.
+        # Setup request and view.
         response = self.client.get('/login/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'login.html')
