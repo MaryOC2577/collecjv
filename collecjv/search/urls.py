91ecda678e917d24bdf3bb44bbd9c7aaa0205050
Maryline <marybot@free.fr>
2024-01-09 17:55:29 +0100

    add search result page

--- collecjv/search/urls.py
+++ collecjv/search/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.SearchView.as_view(), name="search"),
-    path("", views.GameSheet.as_view(), name="gamesheet"),
+   
 ]
--- collecjv/search/views.py
+++ collecjv/search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from collecjv.models import Game
 
 
 class SearchView(View):
@@ -7,7 +8,17 @@ class SearchView(View):
         return render(request, "search.html")
     
 
-class GameSheet(View):
-    def get(self, request):
-        return render(request, "gamesheet.html")
-    
+class GameSearchResult(View):
+    template_name = "result.html"
+    model = Game
+    context_object_name = "games"
+
+    def get_queryset(self):
+        expression = self.request.GET.get("expression", "").title()
+        print("expression :", expression)
+        return Game.objects.filter(name__contains=expression).order_by("id") if expression else []
+
+    def get_context_data(self, **kwargs):
+        kwargs["expression"] = self.request.GET.get("expression", "")
+
+        return super().get_context_data(**kwargs)
