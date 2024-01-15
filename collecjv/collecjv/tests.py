a5709dadc625e7b81245f806d1f49124d2dd1f14
Maryline <marybot@free.fr>
2024-01-15 15:41:17 +0100

    update tests collecjv

--- collecjv/collecjv/tests.py
+++ collecjv/collecjv/tests.py
@@ -5,14 +5,9 @@ from collecjv.models import Company
 
 @pytest.mark.django_db
 class TestCompagny(TestCase):
-    
     def test_compagny(self):
         compagny = Company.objects.create(
-            name="test compagny",
-            area="europe",
-            developer=True,
-            editor=True
-
+            name="test compagny", area="europe", developer=True, editor=True
         )
 
         expected_value = "test compagny"
